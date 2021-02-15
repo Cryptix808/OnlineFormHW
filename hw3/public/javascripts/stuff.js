21763b52eb41ba62544525d24ea40a34aaa849d5
Cryptix808 <miggysabater@gmail.com>
2021-02-14 16:33:33 -1000

    Moved js to new file installed node and tested on website.

---
+++ hw3/public/javascripts/stuff.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $(".button").hover(function(){
+      $(".dropdown").slideToggle();
+      $('#dropdownid a').click(function(){
+        val=$(this).data('value');
+        $('#buttonid').text(val);
+      });
+    });
+  //If the Order button is selected then the options the user chose
+  //is locked out. The notes portion will show and appear message if the user
+  //presses enter.
+    $('#notes').keyup(function(){
+        if($(this).val().toLowerCase().indexOf('vegan') != -1){
+          alert('This product contains dairy!');
+        }
+    });
+    $("#orderButton").click(function(){
+      $("#quantityID").replaceWith("Thank you for your order! Below are your details...");
+      $('#orderButton').hide();
+      $("#quantitySelect").each(function(element){
+        $(this).attr('disabled','disabled')
+      });
+      $("#Plain").each(function(element){
+        $(this).attr('disabled','disabled')
+      });
+      $("#Cherry").each(function(element){
+        $(this).attr('disabled','disabled')
+      });
+      $("#Chocolate").each(function(element){
+        $(this).attr('disabled','disabled')
+      });
+      $('#notes').keyup(function(){
+        if($(this).val().toLowerCase().indexOf('vegan') != -1){
+          alert('This product contains dairy!');
+        }
+      });
+    });
+  });
+
+  eventHandler = function( event ) {
+    /* do stuff */
+    }
+    $(function() {
+    $(".foo").click(eventHandler);
+    });
